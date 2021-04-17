d8705c3607d17da2600f6549800d75ad6662cfd8
smartful <remi_rodrigues@hotmail.com>
2021-04-17 03:28:58 +0200

    add spinner and search form

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import './App.css';
 import axios from 'axios';
 
 function App() {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(false);
-
+  
   useEffect(() => {
+    const urlHost = `https://api.github.com/users`;
+    const url = `${urlHost}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`;
     setLoading(true);
     const fetchData = async () => {
-      const res = await axios.get('https://api.github.com/users');
+      const res = await axios.get(url);
       setUsers(res.data);
       setLoading(false);
     };
@@ -22,6 +25,7 @@ function App() {
     <Fragment>
       <Navbar />
       <div className="container">
+        <Search />
         <Users users={users} loading={loading} />
       </div>
     </Fragment>
--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types'
 
 const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
+  }
+
   return (
     <div style={userStyle}>
       {users.map(user => (
@@ -18,6 +24,11 @@ const userStyle = {
   display: "grid",
   gridTemplateColumns: "repeat(3, 1fr)",
   gridGap: "1rem",
+};
+
+Users.propTypes = {
+  users: PropTypes.array,
+  loading: PropTypes.bool,
 };
 
 export default Users;
