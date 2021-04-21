023a2e7abf7e11d371ebd5037868e48ad5ffbdf9
smartful <remi_rodrigues@hotmail.com>
2021-04-21 03:54:58 +0200

    creation of Not Found page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Alert from './components/layout/Alert';
-import Users from './components/users/Users';
 import User from './components/users/User';
-import Search from './components/users/Search';
 import About from './components/pages/About';
+import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
 import './App.css';
@@ -15,23 +15,16 @@ const App = () => {
     <GithubState>
       <AlertState>
         <Router>
-          <Fragment>
-            <Navbar />
-            <div className="container">
-              <Alert />
-              <Switch>
-                <Route exact path='/' render={props => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
-                <Route path='/about' component={About} />
-                <Route path='/user/:loginUsr' component={User} />
-              </Switch>
-            </div>
-          </Fragment>
+          <Navbar />
+          <div className="container">
+            <Alert />
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path='/about' component={About} />
+              <Route path='/user/:loginUsr' component={User} />
+              <Route component={NotFound} />
+            </Switch>
+          </div>
         </Router>
       </AlertState>
     </GithubState>
